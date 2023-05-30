f38f8d164e54f724c2ab7be7564a3c1a5c37b8fe
nischal gautam <nischalgautam7777@gmail.com>
2023-05-30 20:53:23 +0530

    just some minor changes

---
+++ digitalCard/src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './App.css';
+
+
+
+export default function Footer(){
+    return(
+        <>
+        <nav className="navigations">
+            <button className="linkButtons"><img src="./images/mail.png" alt="" /></button>
+            <button className="linkButtons"><img src="./images/whatsapp.png" alt="" /></button>
+            <button className="linkButtons"><img src="./images/call.png" alt="" /></button>
+            <button className="linkButtons"><img src="./images/linkedin.png" alt="" /></button>
+
+
+        </nav>
+        
+        </>
+    )
+}
---
+++ digitalCard/src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import './App.css';
+
+
+
+
+export default function Header(){
+<>
+
+<div className="upperBanner">
+
+
+    <div className="actualBanner">
+        
+        <img></img>
+
+
+    </div>
+
+
+
+    <img id="pfp">
+    
+    </img>
+
+
+
+</div>
+
+
+</>
+}
