f41aec886df7604da95c9ce996b9a5e47480870b
nischal gautam <nischalgautam7777@gmail.com>
2023-06-12 23:53:24 +0530

    pushing the changes

--- digitalCard/src/components/middleBody.js
+++ digitalCard/src/components/middleBody.js
@@ -8,16 +8,10 @@ export default function middleBody(){
     return(
         <>
         <div className="middleBody">
+            <p className="introLine">Hello there , I am Nischal Gautam</p>
             
-            <ul>
-
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-
-
-            </ul>
+            <p>'m an award-winning content writer who has eight years of experience <br/> creating compelling articles and short stories.
+                 I'm continuously searching for new topics and stories to capture <br/> the attention of new readers. With my knowledge and experience, <br/> I can help you fulfill your content creation goals.</p>
 
 
         </div>
